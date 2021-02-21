7d010ed9c3be7f44ab9a1fc93bd17c0dc0f075e9
Alemaksus <alexander.maksakov@gmai.com>
2021-02-21 17:37:37 +0300

    Dinamic memory work

--- Lesson_5/draft.py
+++ Lesson_5/draft.py
@@ -1,115 +1,159 @@
-import platform
-import sys
-import cProfile
+# import platform
+# import sys
+# import cProfile
+#
+# def profile(func):
+#     """Decorator for run function profile"""
+#
+#     def wrapper(*args, **kwargs):
+#         profile_filename = func.__name__ + '.prof'
+#         profiler = cProfile.Profile()
+#         result = profiler.runcall(func, *args, **kwargs)
+#         profiler.dump_stats(profile_filename)
+#         return result
+#
+#     return wrapper
+#
+#
+# def abs123(q):
+#     for i in range(len(q)):
+#         if q[i] == 'a':
+#             q[i] = 10
+#         elif q[i] == 'b':
+#             q[i] = 11
+#         elif q[i] == 'c':
+#             q[i] = 12
+#         elif q[i] == 'd':
+#             q[i] = 13
+#         elif q[i] == 'e':
+#             q[i] = 14
+#         elif q[i] == 'f':
+#             q[i] = 15
+#     return q
+#
+#
+# def to16(s):
+#     for i in range(len(s)):
+#         if s[i] == 10:
+#             s[i] = 'a'
+#         elif s[i] == 11:
+#             s[i] = 'b'
+#         elif s[i] == 12:
+#             s[i] = 'c'
+#         elif s[i] == 13:
+#             s[i] = 'd'
+#         elif s[i] == 14:
+#             s[i] = 'e'
+#         elif s[i] == 15:
+#             s[i] = 'f'
+#     return s
+#
+#
+# def sum16(x, y):
+#     z = []
+#     xx = abs123(x[::-1])
+#     yy = abs123(y[::-1])
+#     if len(xx) >= len(yy):
+#         for i in range(len(xx) - len(yy)):
+#             yy.append(0)
+#     else:
+#         for i in range(len(yy) - len(xx)):
+#             xx.append(0)
+#     p, zz, zzz = 0, 0, 0
+#     for i in range(len(xx)):
+#         zz = int(xx[i]) + int(yy[i]) + p
+#         if zz >= 16:
+#             p = zz // 16
+#             zzz = zz % 16
+#         else:
+#             zzz = zz
+#             p = 0
+#         z.append(zzz)
+#     if p > 0:
+#         z.append(p)
+#     # print(z, to16(z[::-1]))
+#     return to16(z[::-1])
+#
+#
+# def to10(x):
+#     xx = abs123(x)
+#     tenn = 0
+#     for i in range(len(xx)):
+#         ll = len(xx) - i - 1
+#         s = 16 ** ll
+#         ten = int(xx[i]) * s
+#         tenn += ten
+#     return tenn
+#
+#
+# def mul(x, y):
+#     dd = x
+#     for i in range(to10(y) - 1):
+#         dd = sum16(x, dd)
+#     return dd
+#
+#
+# @profile
+# def main():
+#     a = input("\nВведите первое число в 16 формате: ")
+#     b = input("Введите второе число в 16 формате: ")
+#     # a = 'fff'
+#     # b = 'fff'
+#     aa = list(a)
+#     bb = list(a)
+#     sum = sum16(aa, bb)
+#     print(f"\nСумма чисел {a} и {b}:")
+#     for i in sum:
+#         print(i, end="")
+#     dd = mul(aa, bb)
+#     print(f"\nПроизведение чисел {a} и {b}:")
+#     for i in dd:
+#         print(i, end="")
+#     print(f"\n\nцифры в десятичном формате:\na = {to10(aa)}\nb = {to10(bb)}\nsum = {to10(sum)}\nmul = {to10(dd)}\n\n")
+#
+# if __name__ == '__main__':
+#     main()
 
-def profile(func):
-    """Decorator for run function profile"""
+from collections import defaultdict
+from collections import deque
 
-    def wrapper(*args, **kwargs):
-        profile_filename = func.__name__ + '.prof'
-        profiler = cProfile.Profile()
-        result = profiler.runcall(func, *args, **kwargs)
-        profiler.dump_stats(profile_filename)
-        return result
+num_1 = input('Введите первое число: ').upper()
+num_2 = input('Введите второе число: ').upper()
 
-    return wrapper
-
-
-def abs123(q):
-    for i in range(len(q)):
-        if q[i] == 'a':
-            q[i] = 10
-        elif q[i] == 'b':
-            q[i] = 11
-        elif q[i] == 'c':
-            q[i] = 12
-        elif q[i] == 'd':
-            q[i] = 13
-        elif q[i] == 'e':
-            q[i] = 14
-        elif q[i] == 'f':
-            q[i] = 15
-    return q
+chars = '0123456789ABCDEF'
+table = defaultdict(int)
+counter = 0
+for key in chars:
+    table[key] += counter
+    counter += 1
 
 
-def to16(s):
-    for i in range(len(s)):
-        if s[i] == 10:
-            s[i] = 'a'
-        elif s[i] == 11:
-            s[i] = 'b'
-        elif s[i] == 12:
-            s[i] = 'c'
-        elif s[i] == 13:
-            s[i] = 'd'
-        elif s[i] == 14:
-            s[i] = 'e'
-        elif s[i] == 15:
-            s[i] = 'f'
-    return s
+def dexim(string):
+    dex = 0
+    num = deque(string)
+    num.reverse()
+    for i in range(len(num)):
+        dex += table[num[i]] * 16 ** i
+    return dex
 
 
-def sum16(x, y):
-    z = []
-    xx = abs123(x[::-1])
-    yy = abs123(y[::-1])
-    if len(xx) >= len(yy):
-        for i in range(len(xx) - len(yy)):
-            yy.append(0)
-    else:
-        for i in range(len(yy) - len(xx)):
-            xx.append(0)
-    p, zz, zzz = 0, 0, 0
-    for i in range(len(xx)):
-        zz = int(xx[i]) + int(yy[i]) + p
-        if zz >= 16:
-            p = zz // 16
-            zzz = zz % 16
-        else:
-            zzz = zz
-            p = 0
-        z.append(zzz)
-    if p > 0:
-        z.append(p)
-    # print(z, to16(z[::-1]))
-    return to16(z[::-1])
+def hexim(numb):
+    num = deque()
+    while numb > 0:
+        d = numb % 16
+        for i in table:
+            if table[i] == d:
+                num.append(i)
+        numb //= 16
+    num.reverse()
+    return list(num)
 
 
-def to10(x):
-    xx = abs123(x)
-    tenn = 0
-    for i in range(len(xx)):
-        ll = len(xx) - i - 1
-        s = 16 ** ll
-        ten = int(xx[i]) * s
-        tenn += ten
-    return tenn
+num_1 = dexim(num_1)
+num_2 = dexim(num_2)
 
+sum = hexim(num_1 + num_2)
+mult = hexim(num_1 * num_2)
 
-def mul(x, y):
-    dd = x
-    for i in range(to10(y) - 1):
-        dd = sum16(x, dd)
-    return dd
-
-
-@profile
-def main():
-    a = input("\nВведите первое число в 16 формате: ")
-    b = input("Введите второе число в 16 формате: ")
-    # a = 'fff'
-    # b = 'fff'
-    aa = list(a)
-    bb = list(a)
-    sum = sum16(aa, bb)
-    print(f"\nСумма чисел {a} и {b}:")
-    for i in sum:
-        print(i, end="")
-    dd = mul(aa, bb)
-    print(f"\nПроизведение чисел {a} и {b}:")
-    for i in dd:
-        print(i, end="")
-    print(f"\n\nцифры в десятичном формате:\na = {to10(aa)}\nb = {to10(bb)}\nsum = {to10(sum)}\nmul = {to10(dd)}\n\n")
-
-if __name__ == '__main__':
-    main()
+print(f'Сумма: {sum}')
+print(f'Произведение: {mult}')
--- Lesson_5/les_5_task_2.py
+++ Lesson_5/les_5_task_2.py
@@ -18,44 +18,90 @@
 # d = hex(int(a, 16) * int(b, 16))
 # print(c, d)
 
-# Но, по заданию нужно помучиться
+# Но, по заданию нужно помучиться. Мой вариант:
 
-first = input("Введите первое шестнадатиричное число: ")
-second = input("Введите второе шестнадатиричное число: ")
+# first = input("Введите первое шестнадатиричное число: ")
+# second = input("Введите второе шестнадатиричное число: ")
+#
+# list_of_numbers = [str(i) for i in range(10)] + ['A', 'B', 'C', 'D', 'E', 'F']
+#
+# if len(first) > len(second):
+#   first, second = second, first
+#
+# second = second[::-1]
+# third = []
+#
+# j = -1
+# k = 0
+# for i in second:
+#   one = list_of_numbers.index(i)
+#   two = list_of_numbers.index(first[j])
+#   third.append(list_of_numbers[(one + two + k) % 16])
+#   if (one + two) >= 15:
+#     k = 1
+#   else:
+#     k = 0
+#   j -= 1
+#   if j == -len(first)-1:
+#     break
+#
+# diff = len(second) - len(first)
+#
+# if diff:
+#     for i in second[-diff:]:
+#         third.append(list_of_numbers[(list_of_numbers.index(second[-diff]) + k) % 16])
+#         if list_of_numbers.index(second[-diff]) + 1 >= 15:
+#             k = 1
+#         else:
+#             k = 0
+#
+# if k == 1:
+#   third.append('1')
+#
+# print(third[::-1])
 
-list_of_numbers = [str(i) for i in range(10)] + ['A', 'B', 'C', 'D', 'E', 'F']
+# Правильный вариант от Евгении Хохловой:
 
-if len(first) > len(second):
-  first, second = second, first
+from collections import defaultdict
+from collections import deque
 
-second = second[::-1]
-third = []
+num_1 = input('Введите первое число: ').upper()
+num_2 = input('Введите второе число: ').upper()
 
-j = -1
-k = 0
-for i in second:
-  one = list_of_numbers.index(i)
-  two = list_of_numbers.index(first[j])
-  third.append(list_of_numbers[(one + two + k) % 16])
-  if (one + two) >= 15:
-    k = 1
-  else:
-    k = 0
-  j -= 1
-  if j == -len(first)-1:
-    break
+chars = '0123456789ABCDEF'
+table = defaultdict(int)
+counter = 0
+for key in chars:
+    table[key] += counter
+    counter += 1
 
-diff = len(second) - len(first)
 
-if diff:
-    for i in second[-diff:]:
-        third.append(list_of_numbers[(list_of_numbers.index(second[-diff]) + k) % 16])
-        if list_of_numbers.index(second[-diff]) + 1 >= 15:
-            k = 1
-        else:
-            k = 0
+def dexim(string):
+    dex = 0
+    num = deque(string)
+    num.reverse()
+    for i in range(len(num)):
+        dex += table[num[i]] * 16 ** i
+    return dex
 
-if k == 1:
-  third.append('1')
 
-print(third[::-1])
+def hexim(numb):
+    num = deque()
+    while numb > 0:
+        d = numb % 16
+        for i in table:
+            if table[i] == d:
+                num.append(i)
+        numb //= 16
+    num.reverse()
+    return list(num)
+
+
+num_1 = dexim(num_1)
+num_2 = dexim(num_2)
+
+sum = hexim(num_1 + num_2)
+mult = hexim(num_1 * num_2)
+
+print(f'Сумма: {sum}')
+print(f'Произведение: {mult}')
